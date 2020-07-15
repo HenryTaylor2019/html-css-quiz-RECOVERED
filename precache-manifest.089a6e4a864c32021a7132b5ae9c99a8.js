19543f6f631639b0c57e80745759fd405a100d82
HenryTaylor2019 <henrytaylor@live.co.uk>
2020-07-15 15:54:15 +0100

    Updates

---
+++ precache-manifest.089a6e4a864c32021a7132b5ae9c99a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebb3cd779037267ce9d0ba604dea20c2",
+    "url": "/html-css-quiz-RECOVERED/index.html"
+  },
+  {
+    "revision": "32083128e82e4356facb",
+    "url": "/html-css-quiz-RECOVERED/static/css/main.a42c7ed2.chunk.css"
+  },
+  {
+    "revision": "d67c9dfc247d7499e186",
+    "url": "/html-css-quiz-RECOVERED/static/js/2.898bc2dd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/html-css-quiz-RECOVERED/static/js/2.898bc2dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32083128e82e4356facb",
+    "url": "/html-css-quiz-RECOVERED/static/js/main.f5296605.chunk.js"
+  },
+  {
+    "revision": "2e6d27c20c88df4ecead",
+    "url": "/html-css-quiz-RECOVERED/static/js/runtime-main.dcbf1d3d.js"
+  }
+]);
